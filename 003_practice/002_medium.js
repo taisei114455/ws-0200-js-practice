ffb9bb0b6c912299b0ba524564e7f58eb08449b1
taisei <taiseisunaga@gmail.com>
2023-08-05 02:17:39 +0900

    update

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -10,13 +10,18 @@
  *
  */
 function rotate(str, num) {
-  num = num % str.length; //numがstrの文字列の長さ内になるようにする
+  //numがstrの文字列の長さ内になるようにする
+  num = num % str.length;
 
+  //負のnumを正にする
   if (num < 0) {
-    num = str.length + num;} //負のnumを正にする
+    num = str.length + num;}
 
-    let result1 = str.slice(str.length - num); //文字列の長さをnumで引いた位置から末尾まで
-    let result2 = str.slice(0, str.length - num); //文字列の先頭から文字列の長さをnumで引いた位置まで
+    //文字列の長さをnumで引いた位置から末尾まで
+    let result1 = str.slice(str.length - num); 
+    
+     //文字列の先頭から文字列の長さをnumで引いた位置まで
+    let result2 = str.slice(0, str.length - num);
     return result1 + result2;
 }
 
@@ -32,14 +37,19 @@ function rotate(str, num) {
  *
  */
 function removeVowels(str) {
-  let array = [...str]; //文字列を配列に入れる
-  let vowels = ['a','i','u','e','o','A','I','U','E','O']; //消したい文字
+  //文字列を配列に変える
+  let array = [...str];
 
+  //消したい文字
+  let vowels = ['a','i','u','e','o','A','I','U','E','O']; 
+
+  //array内でvowelsにある文字を削除する
   let result = array.filter(function(sakujo) {
     return ! vowels.includes(sakujo);
-  }); //array内でvowelsにある文字を削除する
+  });
 
-  return result.join('');//配列を文字列に戻す
+  //配列を文字列に戻す
+  return result.join('');
 }
 
 /**
@@ -54,7 +64,28 @@ function removeVowels(str) {
  *
  */
 function countStr(s1, s2) {
+  //s1,s2のどちらか一つでも空であれば0を返す
+  if (!s1 || !s2) {
+    return 0;
+  }
+
+  // カウント用
+  let count = 0;
+  // 検索を開始する位置
+  let index = 0;
+  
+  // indexOfの戻り値が-1でない場合、（s2がs1の中で見つかる限り）ループ
+  while ((index = s1.indexOf(s2, index)) !== -1) {
+    // 見つかった場合１つずつ増やす
+    count++;
+    // 検索の位置をs2の文字数分ずらす
+    index += s2.length;
+  }
+
+  return count;
 }
+
+// 文字列.indexOf(検索文字列 [, 開始インデックス])
 
 /**
  *  引数に与えられたアルファベットの文字列が回文であること
@@ -69,6 +100,19 @@ function countStr(s1, s2) {
  */
 
 function isPalindrome(str) {
+ // 文字列の長さ
+ const len = str.length;
+
+ // 文字列の前半部分（len / 2まで）をループして、文字列の先頭から始まる文字と対応する文字を末尾から比較
+ for (let i = 0; i < len / 2; i++) {
+
+     // 一致しなければfalseを返す
+     if (str[i] !== str[len - 1 - i]) {
+         return false;
+     }
+ }
+ // ループで一致しない箇所がなければtrue
+ return true;
 }
 
 /**
@@ -86,6 +130,28 @@ function isPalindrome(str) {
  *
  */
 function isPrime(num) {
+
+  //1は素数でないのでfalseを返す
+  if (num === 1) {
+    return false;
+
+    //2は素数なのでtrueを返す
+    } else if(num === 2) {
+    return true;
+
+    } else {
+    for (i = 2; i < num; i++) {
+
+      //iで割り切れたらfalse
+      if (num % i === 0) {
+        return false;
+      
+      //i + 1してnumと同じであればtrue、同じで無ければループに戻る
+      } else if (i + 1 === num) {
+        return true;
+      }
+    }
+  }
 }
 
 /**
@@ -104,6 +170,27 @@ function isPrime(num) {
  *
  */
 function sumWithout4andNext(array) {
+  // 返却用
+  let result = 0;
+  // skipはfalseでスタート
+  let skip = false;
+
+  for (let i = 0; i < array.length; i++) {
+    // array[i]が4のときは、resultに足さずにskipをtureにする
+    if (array[i] === 4) {
+      skip = true;
+
+    // skipがfalseであればresultにarray[i]を足す
+    } else if (!skip) {
+      result += array[i];
+
+    // 上二つの条件に引っかからない場合（4の次の数字）はresultに足さずにskipをfalseに戻す
+    } else {
+      skip = false;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
